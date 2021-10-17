61353da157f1cefb3be4e7532ba85a79a225cb08
h3R <hermann.plass@gmail.com>
2021-10-18 01:29:14 +0200

    Initial Commit

---
+++ observable.js
@@ -0,0 +1,26 @@
+//https://stackoverflow.com/questions/29333017/ecmascript-6-class-destructor
+
+function Observable() {
+    this.events = { open: new Set(), close: new Set() };
+}
+
+Observable.prototype.on = function (type, f) {
+    if(!this.events.hasOwnProperty(type))
+        this.events[type] = new Set();
+    this.events[type].add(f);
+};
+
+Observable.prototype.emit = function (type, ...args) {
+    this.events[type]?.forEach(f => f(...args));
+};
+
+Observable.prototype.off = function (type, f) {
+    this.events[type]?.delete(f);
+};
+
+Observable.prototype.removeAll = function (type) {
+    delete this.events[type];
+};
+
+window.Observable = Observable;
+export default Observable;
